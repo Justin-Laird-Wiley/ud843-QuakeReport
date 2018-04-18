72e6f4b5d5919d022821564dc8f2b39529a95528
jlairdwiley <jlairdwiley@yahoo.com>
2018-04-17 23:56:00 -0400

    3rd commit -- fix comments in EarthquakeArrayAdapter.java

--- app/src/main/java/com/example/android/quakereport/EarthquakeArrayAdapter.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeArrayAdapter.java
@@ -49,28 +49,28 @@ public class EarthquakeArrayAdapter extends ArrayAdapter<Earthquake> {
                     R.layout.list_item, parent, false);
         }
 
-        // Get the {@link AndroidFlavor} object located at this position in the list
+        // Get the {@link Earthquake} object located at this position in the list
         Earthquake currentEarthquake = getItem(position);
 
-        // Find the TextView in the list_item.xml layout with the ID version_name
+        // Find the TextView in the list_item.xml layout with earthquake_location
         TextView locationTextView = (TextView) listItemView.findViewById(R.id.earthquake_location);
-        // Get the version name from the current AndroidFlavor object and
-        // set this text on the name TextView
+        // Get the earthquake location from the current Earthquake object and
+        // set this text on the location TextView
         locationTextView.setText(currentEarthquake.getLocation());
 
-        // Find the TextView in the list_item.xml layout with the ID version_number
+        // Find the TextView in the list_item.xml layout with the earthquake_date
         TextView dateTextView = (TextView) listItemView.findViewById(R.id.earthquake_date);
-        // Get the version number from the current AndroidFlavor object and
-        // set this text on the number TextView
+        // Get the earthquake date from the current Earthquake object and
+        // set this text on the date TextView
         dateTextView.setText(currentEarthquake.getDate());
 
-        // Find the ImageView in the list_item.xml layout with the ID list_item_icon
+        // Find the TextView in the list_item.xml layout with the ID list_item_icon
         TextView magnitudeView = (TextView) listItemView.findViewById(R.id.earthquake_magnitude);
-        // Get the image resource ID from the current AndroidFlavor object and
-        // set the image to iconView
+        // Get the earthquake magnitude from the current Earthquake object and
+        // set this text on the magnitude TextView
         magnitudeView.setText(String.valueOf(currentEarthquake.getMagnitude()));
 
-        // Return the whole list item layout (containing 2 TextViews and an ImageView)
+        // Return the whole list item layout (containing 3 TextViews)
         // so that it can be shown in the ListView
         return listItemView;
     }
